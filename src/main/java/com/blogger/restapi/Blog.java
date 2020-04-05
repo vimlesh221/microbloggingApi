21e0558a562386800730fd8d8fe75f977551c3f6
Vimlesh Narayan <vimlesh.narayan@synapse.com>
2020-04-05 17:24:35 +0530

    integrated mysql database

--- src/main/java/com/blogger/restapi/Blog.java
+++ src/main/java/com/blogger/restapi/Blog.java
@@ -1,27 +1,59 @@
 package com.blogger.restapi;
 
+import javax.persistence.*;
+
+@Entity
 public class Blog {
-	String title;
-    String data;
-    String label;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String title;
+    private String content;
+
+    public Blog() {  }
+
+    public Blog(String title, String content) {
+        this.setTitle(title);
+        this.setContent(content);
+    }
+
+    public Blog(int id, String title, String content) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setContent(content);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getData() {
-		return data;
-	}
-	public void setData(String data) {
-		this.data = data;
-	}
-	public String getLabel() {
-		return label;
-	}
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	
-    
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
